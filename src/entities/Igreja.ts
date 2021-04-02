d7f6262b684f032351a4118b990ef12bbea71dc3
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-04-02 19:17:57 -0300

    Relation IgrejaMatriz-Filiais finalized

--- src/entities/Igreja.ts
+++ src/entities/Igreja.ts
@@ -5,35 +5,38 @@ import { Usuario } from "./Usuario";
 @Entity('igreja')
 class Igreja {
 
-    // constructor(
-    //     nomeIgreja: string, 
-    //     tipoIgreja: number, 
-    //     logoIgreja: string, 
-    //     matrizIgreja: boolean, 
-    //     qtdmembros: number, 
-    //     rua: string, 
-    //     bairro: string, 
-    //     numero: string, 
-    //     complemento: string,
-    //     cidade: string,
-    //     estado: string,
-    //     pais: string,
-    //     usuario: Usuario
-    //     ) {
-    //         this.nome_igreja = nomeIgreja,
-    //         this.tipo_igreja = tipoIgreja,
-    //         this.logo_igreja = logoIgreja,
-    //         this.matriz_igreja = matrizIgreja,
-    //         this.qtd_membro_igreja = qtdmembros,
-    //         this.rua_igreja = rua,
-    //         this.bairro_igreja = bairro,
-    //         this.numero_residencia = numero,
-    //         this.complemento = complemento,
-    //         this.cidade_igreja = cidade,
-    //         this.estado_igreja = estado,
-    //         this.pais_igreja = pais,
-    //         this.usuario = usuario
-    // }
+    constructor(
+        nomeIgreja: string, 
+        tipoIgreja: boolean, 
+        logoIgreja: string, 
+        matrizIgreja: boolean, 
+        qtdmembros: number, 
+        rua: string, 
+        bairro: string, 
+        numero: number, 
+        complemento: string,
+        cidade: string,
+        estado: string,
+        pais: string,
+        usuario: Usuario,
+        atividade: Atividade[]
+        ) {
+            this.nome_igreja = nomeIgreja,
+            this.tipo_igreja = tipoIgreja,
+            this.logo_igreja = logoIgreja,
+            this.matriz_igreja = matrizIgreja,
+            this.qtd_membro_igreja = qtdmembros,
+            this.rua_igreja = rua,
+            this.bairro_igreja = bairro,
+            this.numero_residencia = numero,
+            this.complemento_residencia_igreja = complemento,
+            this.cidade_igreja = cidade,
+            this.estado_igreja = estado,
+            this.pais_igreja = pais,
+            this.usuario = usuario,
+            this.atividades = atividade
+
+    }
 
     @PrimaryGeneratedColumn('uuid')
     readonly id_igreja: string;
@@ -53,12 +56,11 @@ class Igreja {
     @Column()
     matriz_igreja: boolean;
 
-    @ManyToOne(() => Igreja, filial => filial.filial)
-    @Column('uuid')
-    igreja: Igreja
+    @ManyToOne(() => Igreja, igreja => igreja.filiais)
+    igrejaMatriz: Igreja
 
-    @OneToMany(() => Igreja, igreja => igreja.igreja)
-    filial: Igreja[]
+    @OneToMany(() => Igreja, igreja => igreja.igrejaMatriz)
+    filiais: Igreja[]
 
     @Column()
     qtd_membro_igreja: number;
